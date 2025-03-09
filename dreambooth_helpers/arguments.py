157630a0bffad6a6f13f2862d63d9444575b0a38
Chunk Norrmis <128258022+ChunkNorrmis@users.noreply.github.com>
2025-03-09 15:49:13 -0400

    Update arguments.py

--- dreambooth_helpers/arguments.py
+++ dreambooth_helpers/arguments.py
@@ -155,7 +155,7 @@ def parse_arguments() -> JoePennaDreamboothConfigSchemaV1:
             default="bicubic",
             required=False
         )
-        
+
         return parser
 
     parser = _get_parser()
@@ -191,8 +191,111 @@ def parse_arguments() -> JoePennaDreamboothConfigSchemaV1:
 
     return config
 
-def split_parser():
+def split_parser() -> parse_arguments:
     parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--config_file_path",
+        type=str,
+        required=False,
+        default=None,
+        help="A config file containing all of your variables"
+    )
+    parser.add_argument(
+        "--project_name",
+        type=str,
+        required=False,
+        default=None,
+        help="Name of the project"
+    )
+    parser.add_argument(
+        "--debug",
+        type=bool,
+        nargs="?",
+        const=True,
+        default=False,
+        help="Enable debug logging",
+    )
+    parser.add_argument(
+        "--seed",
+        type=int,
+        default=23,
+        help="seed for seed_everything",
+    )
+    parser.add_argument(
+        "--max_training_steps",
+        type=int,
+        required=False,
+        help="Number of training steps to run"
+    )
+    parser.add_argument(
+        "--token",
+        type=str,
+        required=False,
+        help="Unique token you want to represent your trained model. Ex: firstNameLastName."
+    )
+    parser.add_argument(
+        "--token_only",
+        type=bool,
+        const=True,
+        default=False,
+        nargs="?",
+        help="Train only using the token and no class."
+    )
+    parser.add_argument(
+        "--training_model",
+        type=str,
+        required=False,
+        help="Path to model to train (model.ckpt)"
+    )
+    parser.add_argument(
+        "--training_images",
+        type=str,
+        required=False,
+        help="Path to training images directory"
+    )
+    parser.add_argument(
+        "--regularization_images",
+        type=str,
+        required=False,
+        help="Path to directory with regularization images"
+    )
+    parser.add_argument(
+        "--class_word",
+        type=str,
+        required=False,
+        help="Match class_word to the category of images you want to train. Example: 'man', 'woman', 'dog', or 'artstyle'."
+    )
+    parser.add_argument(
+        "--flip_p",
+        type=float,
+        required=False,
+        default=0.0,
+        help="Flip Percentage "
+                "Example: if set to 0.5, will flip (mirror) your training images 50% of the time."
+                "This helps expand your dataset without needing to include more training images."
+                "This can lead to worse results for face training since most people's faces are not perfectly symmetrical."
+    )
+    parser.add_argument(
+        "--learning_rate",
+        type=float,
+        required=False,
+        default=1.0e-06,
+        help="Set the learning rate. Defaults to 1.0e-06 (0.000001).  Accepts scientific notation."
+    )
+    parser.add_argument(
+        "--save_every_x_steps",
+        type=int,
+        required=False,
+        default=2000,
+        help="Saves a checkpoint every x steps"
+    )
+    parser.add_argument(
+        "--gpu",
+        type=int,
+        default=0,
+        required=False,
+        help="Specify a GPU other than 0 to use for training.  Multi-GPU support is not currently implemented."
+    )
     parser.add_argument(
         "--batch_size",
         type=int,
@@ -232,4 +335,3 @@ def split_parser():
     )
 
     return parser
-
