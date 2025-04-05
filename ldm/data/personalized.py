62efd02de6f79d5caa3a99fdb087a06e97c5da96
Chunk Norrmis <128258022+ChunkNorrmis@users.noreply.github.com>
2025-04-05 06:14:27 -0400

    Update personalized.py

--- ldm/data/personalized.py
+++ ldm/data/personalized.py
@@ -14,58 +14,55 @@ per_img_token_list = [
 class PersonalizedBase(Dataset):
     def __init__(
         self,
-        data_root,
-        size,
-        repeats,
         set,
+        reg,
+        data_root,
         placeholder_token,
         coarse_class_text,
-        flip_p,
+        repeats,
+        size,
         interpolation,
+        center_crop,
+        flip_p,
         token_only,
         per_image_tokens,
-        center_crop,
-        reg=None,
-        mixing_prob=0.25
+        mixing_prob
     ):
-        super(PersonalizedBase).__init__()
-        
+        super().__init__()
+        self.set = set
+        self.reg = reg
         self.data_root = data_root
         self.image_paths = find_images(self.data_root)
-        self.num_images = len(self.image_paths)
-        self._length = self.num_images
         self.placeholder_token = placeholder_token
-        self.token_only = token_only
-        self.per_image_tokens = per_image_tokens
-        self.center_crop = center_crop
-        self.mixing_prob = mixing_prob
         self.coarse_class_text = coarse_class_text
+        self.repeats = repeats
         self.size = size
         self.flip = transforms.RandomHorizontalFlip(p=flip_p)
-
-        if per_image_tokens:
-            assert self.num_images < len(
-                per_img_token_list), f"Can't use per-image tokens when the training set contains more than {len(per_img_token_list)} tokens. To enable larger sets, add more tokens to 'per_img_token_list'."
-
-        if set == "train":
-            self._length = self.num_images * repeats
-
+        self.center_crop = center_crop        
+        self.token_only = token_only
+        self.per_image_tokens = per_image_tokens
+        self.mixing_prob = mixing_prob
         self.interpolation = {
             "bilinear": PIL.Image.BILINEAR,
             "bicubic": PIL.Image.BICUBIC,
             "lanczos": PIL.Image.LANCZOS
         }[interpolation]
+        
+        if self.per_image_tokens:
+            assert self.__len__() < len(per_img_token_list), f"Can't use per-image tokens when the training set contains more than {len(per_img_token_list)} tokens. To enable larger sets, add more tokens to 'per_img_token_list'."
 
-        self.reg = reg
+        if self.set == "train":
+            self._length = self.__len__() * self.repeats
+
         if self.reg and self.coarse_class_text:
             self.reg_tokens = OrderedDict([('C', self.coarse_class_text)])
 
     def __len__(self):
-        return self._length
+        return len(self.image_paths)
 
     def __getitem__(self, i):
         example = {}
-        image_path = self.image_paths[i % self.num_images]
+        image_path = self.image_paths[i % self.__len__()]
         image = Image.open(image_path)
 
         if not image.mode == "RGB":
