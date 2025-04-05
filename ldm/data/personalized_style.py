183ba9d51a6b224000e131712b6ed7801588ee0c
Chunk Norrmis <128258022+ChunkNorrmis@users.noreply.github.com>
2025-04-05 07:27:17 -0400

    Update personalized_style.py

--- ldm/data/personalized_style.py
+++ ldm/data/personalized_style.py
@@ -54,46 +54,44 @@ per_img_token_list = [
 ]
 
 class PersonalizedBase(Dataset):
-    def __init__(self,
-                 data_root,
-                 size,
-                 repeats,
-                 interpolation,
-                 flip_p,
-                 set,
-                 placeholder_token,
-                 per_image_tokens=False,
-                 center_crop=False,
-                 ):
-
+    def __init__(
+        self,
+        data_root,
+        size,
+        repeats,
+        interpolation,
+        flip_p,
+        set,
+        placeholder_token,
+        per_image_tokens=False,
+        center_crop=False
+    ):
+        super().__init__()
+        self.set = set
+        self.repeats = repeats
+        self.size = size
         self.data_root = data_root
-
         self.image_paths = [os.path.join(self.data_root, file_path) for file_path in os.listdir(self.data_root)]
-
-        # self._length = len(self.image_paths)
-        self.num_images = len(self.image_paths)
-        self._length = self.num_images 
-
+        self.num_images = self.__len__()
         self.placeholder_token = placeholder_token
-
         self.per_image_tokens = per_image_tokens
         self.center_crop = center_crop
-
+        self.flip = transforms.RandomHorizontalFlip(p=flip_p)
+        self.interpolation = {
+            "bilinear": PIL.Image.BILINEAR,
+            "bicubic": PIL.Image.BICUBIC,
+            "lanczos": PIL.Image.LANCZOS,
+        }[interpolation]
+        
         if per_image_tokens:
             assert self.num_images < len(per_img_token_list), f"Can't use per-image tokens when the training set contains more than {len(per_img_token_list)} tokens. To enable larger sets, add more tokens to 'per_img_token_list'."
 
-        if set == "train":
-            self._length = self.num_images * repeats
+        if self.set == "train":
+            self.num_images = self.__len__() * self.repeats
 
-        self.size = size
-        self.interpolation = {"bilinear": PIL.Image.BILINEAR,
-                              "bicubic": PIL.Image.BICUBIC,
-                              "lanczos": PIL.Image.LANCZOS,
-                              }[interpolation]
-        self.flip = transforms.RandomHorizontalFlip(p=flip_p)
 
     def __len__(self):
-        return self._length
+        return len(self.image_paths)
 
     def __getitem__(self, i):
         example = {}
@@ -108,21 +106,23 @@ class PersonalizedBase(Dataset):
             text = random.choice(imagenet_templates_small).format(self.placeholder_token)
             
         example["caption"] = text
-        img = np.array(image).astype(np.uint8)
         
-        if self.center_crop:
+        if self.center_crop and not image.width == image.height:
+            img = np.array(image).astype(np.uint8)
             crop = min(img.shape[0], img.shape[1])
             h, w, = img.shape[0], img.shape[1]
             img = img[(h - crop) // 2:(h + crop) // 2,
-                (w - crop) // 2:(w + crop) // 2]
-
-        image = Image.fromarray(img)
+                      (w - crop) // 2:(w + crop) // 2]
+            image = Image.fromarray(img)
         
         if not (self.size, self.size) == image.size:
-            image = image.resize((self.size, self.size), resample=self.interpolation, reducing_gap=3)
-            image = ImageEnhance.Sharpness(image).enhance(1.2)
+            image = image.resize(
+                (self.size, self.size),
+                resample=self.interpolation,
+                reducing_gap=3)
+            image = ImageEnhance.Sharpness(image).enhance(1.3)
 
+        image = self.flip(image)
         image = np.array(image).astype(np.uint8)
-        
         example["image"] = (image / 127.5 - 1.0).astype(np.float32)
         return example
