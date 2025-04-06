03270e6199a4ce6bbad17b60c66f4f22b66a42a4
Chunk Norrmis <128258022+ChunkNorrmis@users.noreply.github.com>
2025-04-06 18:01:58 -0400

    Update personalized.py

--- ldm/data/personalized.py
+++ ldm/data/personalized.py
@@ -33,7 +33,8 @@ class PersonalizedBase(Dataset):
         self.reg = reg
         self.data_root = data_root
         self.image_paths = find_images(self.data_root)
-        self.image_count = len(self.image_paths)
+        self._len = len(self.image_paths)
+        self.image_count = self._len
         self.placeholder_token = placeholder_token
         self.coarse_class_text = coarse_class_text
         self.repeats = repeats
@@ -50,16 +51,16 @@ class PersonalizedBase(Dataset):
         }[interpolation]
                 
         if self.per_image_tokens:
-            assert self.image_count < len(per_img_token_list), f"Can't use per-image tokens when the training set contains more than {len(per_img_token_list)} tokens. To enable larger sets, add more tokens to 'per_img_token_list'."
+            assert self._len < len(per_img_token_list), f"Can't use per-image tokens when the training set contains more than {len(per_img_token_list)} tokens. To enable larger sets, add more tokens to 'per_img_token_list'."
 
         if self.set == "train":
-            self.image_count = self.image_count * self.repeats
+            self.image_count = self._len * self.repeats
 
         if self.reg and self.coarse_class_text:
             self.reg_tokens = OrderedDict([('C', self.coarse_class_text)])
 
     def __len__(self):
-        return len(self.image_paths)
+        return self._image_count
 
     def __getitem__(self, i):
         example = {}
